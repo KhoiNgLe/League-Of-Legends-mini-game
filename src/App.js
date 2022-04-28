0a5a4940db681f9077b0c9acdb73e33426531953
KhoiNgLe <nguy0572@algonquinlive.com>
2022-04-27 22:21:15 -0400

    Add card grid

--- src/App.js
+++ src/App.js
@@ -1,12 +1,48 @@
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+
+const cardImages = [
+  { src: "/img/ashe.jpg" },
+  { src: "/img/lulu.jpg" },
+  { src: "/img/riven.jpg" },
+  { src: "/img/singed.jpg" },
+  { src: "/img/soraka.jpg" },
+  { src: "/img/volibear.jpg" },
+];
 
 function App() {
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
+
+  // shuffle cards for new game
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }));
+
+    setCards(shuffledCards);
+    setTurns(0);
+  };
+
+  console.log(cards, turns);
+
   return (
     <div className="App">
-      <h1>League of Legends Match</h1>
-      <button>New Game</button>
+      <h1>League of Legends Mini Game</h1>
+      <button onClick={shuffleCards}>New Game</button>
+
+      <div className="card-grid">
+        {cards.map(card => (
+          <div className="card" key={card.id}>
+            <div>
+              <img className="front" src={card.src} alt="card front" />
+              <img className="back" src="/img/cover.jpg" alt="card back" />
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
 
-export default App
+export default App;
